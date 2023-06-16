97d1ff93c444ed95b78b12980f8a5062e056b12b
Matthew <matthewtoo5@googlemail.com>
2023-06-17 00:47:45 +0100

    Fancy UI Template
    Potentially bloated but nice!

---
+++ TurboTweak/Models/AppConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace TurboTweak.Models
+{
+    public class AppConfig
+    {
+        public string ConfigurationsFolder { get; set; }
+
+        public string AppPropertiesFileName { get; set; }
+    }
+}
